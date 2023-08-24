a50dd4dec66c87e9e63b9fc4cf94524f468819be
Rasmus T. Jørgensen <rasmus.tif@gmail.com>
2023-08-24 11:17:06 +0200

    bcrypt

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ const app = express();
 //methods for file, dirpath
 const path = require('path')
 
+//bcrypt
+const bcrypt = require("bcrypt")
+const saltRounds = 13
+const password = "test"
+
+
+
 //here we insert ejs
 app.set('view engine', 'ejs')
 
@@ -14,6 +21,24 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req,res) =>{
     res.render('home')
 })
+
+
+bcrypt
+  .hash(password, saltRounds)
+  .then(hash => {
+    console.log('Hash ', hash)
+  })
+  .catch(err => console.error(err.message))
+
+function validateUser(hash) {
+    bcrypt
+      .compare(password, hash)
+      .then(res => {
+        console.log("Her", res) // return true
+      })
+      .catch(err => console.error(err.message))        
+}
+validateUser('$2b$10$mEhFMtBUw50ollEDODZV7.7si3Cw.fwXyZPnSkNKreXjViH9JL1Ta')
 
 // start server
 app.listen(8080, () => {
